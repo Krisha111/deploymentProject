297ce7f55c1b66e40519d6668d5178b26e7e5992
recipeproject <recipeproject@gmail.com>
2023-06-17 16:43:05 +0530

    Initial commit

---
+++ models/model.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose')
+
+const happy =new mongoose.Schema({
+   
+    name: {
+        type: String,
+      },
+      hobbies:{
+        type: String,
+    },
+    email:{
+        type: String,
+    },
+    phone:{
+        type : String
+       }
+ })
+
+ const Category = mongoose.model("Category", happy)
+module.exports=Category;
